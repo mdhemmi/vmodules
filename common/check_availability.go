abd9435946095c8153a6283e282d4a8300b782f5
Michael Hempel <mhempel@vmware.com>
2023-02-07 12:16:19 +0100

    add multiple functions

---
+++ common/check_availability.go
@@ -0,0 +1,22 @@
+package vmodules
+
+func Check_Service_Availability(target string, port int) bool {
+	//fmt.Print("Check if Target Service is available: ")
+	checkTimeout := 5
+	serverAddress := net.JoinHostPort(target, strconv.Itoa(port))
+	timeout := time.Second * time.Duration(checkTimeout)
+	tcpConn, tcpErr := net.DialTimeout("tcp", serverAddress, timeout)
+	tcpResult := "FAIL"
+	if tcpErr == nil {
+		tcpResult = "OK"
+	}
+	if tcpResult == "OK" {
+		tcpConn.Close()
+		//fmt.Println("OK")
+		//fmt.Println("")
+		return true
+	} else {
+		//fmt.Println("FAIL")
+		return false
+	}
+}
---
+++ common/get_func_name.go
@@ -0,0 +1,7 @@
+package vmodules
+
+func GetCurrentFuncName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return runtime.FuncForPC(pc).Name()
+	//return fmt.Sprintf("%s", runtime.FuncForPC(pc).Name())
+}
