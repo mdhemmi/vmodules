b6486a3f0c78b451c1344a48cafd11bcf2ea4522
Michael Hempel <hemmi@xmsoft.de>
2023-02-07 14:07:34 +0100

    Delete getfuncname.go

--- getfuncname/getfuncname.go
+++
@@ -1,9 +0,0 @@
-package getfuncname
-
-import "runtime"
-
-func GetCurrentFuncName() string {
-	pc, _, _, _ := runtime.Caller(1)
-	return runtime.FuncForPC(pc).Name()
-	//return fmt.Sprintf("%s", runtime.FuncForPC(pc).Name())
-}
