f749c8bc329a0fb7e3fc9a092fe090f05716553e
Michael Hempel <mhempel@vmware.com>
2023-02-07 12:08:42 +0100

    add loginsight.go

---
+++ vmware/vrli/loginsight.go
@@ -0,0 +1,62 @@
+package vmodules
+
+func Login(username string, password string, provider string, vrli string) {
+
+	// curl -k -X POST https://loginsight.example.com:9543/api/v1/sessions \
+	//     -d '{"username":"admin","password":"Secret!","provider":"Local"}'
+
+	// TODO: This is insecure; use only in dev environments.
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	var requestbody = "{\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"provider\":\"" + provider + "\"}"
+
+	body := strings.NewReader(requestbody)
+	req, err := http.NewRequest("POST", "https://"+vrli+":9543/api/v1/sessions", body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		// handle err
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(content), &result)
+	//spew.Dump(result)
+	sessionId = fmt.Sprint(result["sessionId"])
+
+}
+
+func Query(sessionId string) {
+	// GET /api/v1/aggregated-events/{+path}
+
+	// TODO: This is insecure; use only in dev environments.
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	body := strings.NewReader("")
+	req, err := http.NewRequest("GET", "https://"+vrli+":9543/api/v1/events", body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+sessionId)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		// handle err
+	}
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(content), &result)
+	spew.Dump(result)
+}
